137c1d79bd2a41d7bf74272d3560543bc53d06f8
Oleg Pustovit <me@opustovit.com>
2026-09-21 14:00:35 +0200

    chore(release): prepare v0.3.23

--- integration/installer_test.go
+++ integration/installer_test.go
@@ -32,8 +32,8 @@ func TestReleaseInstallerVersionsMatchCLI(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		if !strings.Contains(string(contents), "0.3.22") {
-			t.Errorf("%s does not contain release version 0.3.22", relative)
+		if !strings.Contains(string(contents), "0.3.23") {
+			t.Errorf("%s does not contain release version 0.3.23", relative)
 		}
 	}
 }
--- internal/cli/run.go
+++ internal/cli/run.go
@@ -19,7 +19,7 @@ import (
 	packSync "github.com/OlegHQ/agentpack/internal/sync"
 )
 
-var Version = "0.3.22"
+var Version = "0.3.23"
 
 type Runner struct {
 	Stdout, Stderr io.Writer
